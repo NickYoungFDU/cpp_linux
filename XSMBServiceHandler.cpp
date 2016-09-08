d85f021ea0ed49aaf140c33450d3d6ee41c2d73b
Xufan Yang <xufyan@microsoft.com>
2016-09-08 16:29:39 +0800

    Try...

--- XSMBServiceHandler.cpp
+++ XSMBServiceHandler.cpp
@@ -115,11 +115,11 @@ namespace azure {
 
 			void XSMBServiceHandler::WriteFile(LinuxFileResponse& _return, const std::string& filePath, const std::string& bufToSend) {
 				printf("WriteFile\n");
-				//boost::filesystem::path file(filePath);
+				boost::filesystem::path file(filePath);
 				try {
-					//boost::filesystem::fstream fs;
-					std::ofstream out;
-					out.open(filePath.c_str(), std::ios::app);
+					boost::filesystem::ofstream fs;
+					//std::ofstream out;
+					//out.open(filePath.c_str(), std::ios::app);
 					/*
 					if (boost::filesystem::exists(file)) {
 						fs.open(file, boost::filesystem::fstream::ate);
@@ -128,11 +128,11 @@ namespace azure {
 						fs.open(file, boost::filesystem::fstream::out);
 					}
 					*/
-					//fs.open(file, boost::filesystem::fstream::ate | boost::filesystem::fstream::out);
+					fs.open(file, boost::filesystem::fstream::app);
 					//fs.seekp(0, boost::filesystem::fstream::ios_base::end);
-					//fs.write(bufToSend.c_str(), bufToSend.length());
-					out << bufToSend;
-					out.close();
+					fs.write(bufToSend.c_str(), bufToSend.length());
+					//out << bufToSend;
+					//out.close();
 					SetResponse(_return, true, "Successfully write to " + filePath);
 				}
 				catch (const std::exception& ex) {
