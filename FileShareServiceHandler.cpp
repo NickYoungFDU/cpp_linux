af2fd184ac08be498c3d9eaa061fcba13602a43f
Xufan Yang <xufyan@microsoft.com>
2016-09-20 15:17:40 +0800

    exp

--- FileShareServiceHandler.cpp
+++ FileShareServiceHandler.cpp
@@ -24,10 +24,10 @@ namespace azure {
 					std::string mountCommand = "mount -t cifs " + smbShareAddress + " " + mountPoint + " -o vers='" + xsmbVersion + "',username='" + username + "',password='" + password + "',dir_mode=0777,file_mode=0777";
 					std::string ret = exec(mountCommand.c_str());
 					std::cout << ret << std::endl;
-					SetResponse(_return, true, "Successfully mapped " + mountPoint + " to " + smbShareAddress);
+					set_response(_return, true, "Successfully mapped " + mountPoint + " to " + smbShareAddress);
 				}
 				catch (const std::exception& ex) {
-					throw GetException(ex.what(), OperationType::MapFileShare);
+					throw set_exception(ex.what(), OperationType::MapFileShare);
 				}
 			}
 
@@ -38,10 +38,10 @@ namespace azure {
 					std::string unmountCommand = "umount " + mountPoint;
 					std::string ret = exec(unmountCommand.c_str());
 					std::cout << ret << std::endl;
-					SetResponse(_return, true, "Successfully unmapped " + mountPoint);
+					set_response(_return, true, "Successfully unmapped " + mountPoint);
 				}
 				catch (const std::exception& ex) {
-					throw GetException(ex.what(), OperationType::UnmapFileShare);
+					throw set_exception(ex.what(), OperationType::UnmapFileShare);
 				}
 			}
 
