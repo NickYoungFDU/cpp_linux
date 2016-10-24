d429012d2718330df9fcac5e12e04e89ba9695c8
Xufan Yang <xufyan@microsoft.com>
2016-10-24 08:25:29 +0100

    unit test

---
+++ UnitTest.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdexcept>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+int OpenFileHandle(std::string filePath) {
+	int fd = open(filePath.c_str(), O_RDWR);
+	if (fd == -1) {
+		printf("open() failed with error[%s]\n", strerror(errno));		
+		return;
+	}
+}
+
+int CreateFile(std::string filePath) {
+	boost::filesystem::path file(filePath);
+	int fd = creat(filePath.c_str(), 0777);
+	int fileSize = 64;
+	boost::filesystem::resize_file(file, (uintmax_t)fileSize);
+	close(fd);
+}
+
+int LockFileRangeTest(std::string filePath) {
+	if (!boost::filesystem::exists(boost::filesystem::path(filePath)))
+		CreateFile(filePath);
+	int fd = OpenFileHandle(filePath);
+
+	/* Lock file range [0, 4). Exclusive. */
+	struct flock exLock;
+	memset(&exLock, 0, sizeof(exLock));
+	exLock.l_type = F_WRLCK;
+	exLock.l_whence = SEEK_SET;
+	exLock.l_start = 0;
+	exLock.l_len = 4;
+	int result = fcntl(fd, F_SETLK, &lock);
+
+	try {
+		FILE* file = fopen(filePath.c_str(), "rb+");
+		fseek(file, 0, SEEK_SET);
+		fwrite("abcd", sizeof(char), 4, file);
+	}
+	catch (const std::exception &ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
