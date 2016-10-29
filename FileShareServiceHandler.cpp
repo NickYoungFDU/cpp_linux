ac05eaf43e3d1f56a096614d092bac00403145ea
Xufan Yang <xufyan@microsoft.com>
2016-10-29 12:24:55 +0100

    test

--- FileShareServiceHandler.cpp
+++ FileShareServiceHandler.cpp
@@ -35,13 +35,9 @@ namespace azure {
 				// Your implementation goes here
 				printf("UnmapFileShare");
 				try {
-					std::string unmountCommand = "umount " + mountPoint + " &> tmp.txt";
-					//exec(unmountCommand.c_str());
-					system(unmountCommand.c_str());
-					std::string ret;
-					std::fstream fs;
-					fs.open("tmp.txt", std::ios::in);
-					std::getline(fs, ret);
+					//std::string unmountCommand = "umount " + mountPoint;
+					std::string unmountCommand = "ls -l";
+					std::string ret = exec(unmountCommand.c_str());
 					std::cout << ret << std::endl;
 					set_response(_return, true, "Successfully unmapped " + mountPoint);
 				}
