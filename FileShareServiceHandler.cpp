7125a9b56ee222651dd168101174e83fc2c674bb
Xufan Yang <xufyan@microsoft.com>
2016-11-08 20:52:32 +0800

    add log

--- FileShareServiceHandler.cpp
+++ FileShareServiceHandler.cpp
@@ -14,16 +14,18 @@ namespace azure {
 		namespace cpp_linux
 		{
 			void FileShareServiceHandler::MapFileShare(LinuxFileResponse& _return, const std::string& smbShareAddress, const std::string& username, const std::string& password, const std::string& mountPoint) {				
-				printf("MapFileShare");
+				BOOST_LOG(lg) << "Calling [MapFileShare]";
 				boost::filesystem::path mountPointPath(mountPoint);
 				try {
 					if (!boost::filesystem::exists(mountPointPath)) {
+						BOOST_LOG(lg) << "[MapFileShare] " << mountPoint << " does not exist, creating it..";
 						boost::filesystem::create_directory(mountPointPath);
 					}
 					std::string xsmbVersion = "3.0";
-					std::string mountCommand = "mount -t cifs " + smbShareAddress + " " + mountPoint + " -o vers='" + xsmbVersion + "',username='" + username + "',password='" + password + "',dir_mode=0777,file_mode=0777";
+					std::string mountCommand = "mount -t cifs " + smbShareAddress + " " + mountPoint + " -o vers='" + xsmbVersion + "',username='" + username + "',password='" + password + "',dir_mode=0777,file_mode=0777 2>&1";
+					BOOST_LOG(lg) << "[MapFileShare] Excecuting '" << mountCommand << "'";
 					std::string ret = exec(mountCommand.c_str());
-					std::cout << ret << std::endl;
+					BOOST_LOG(lg) << "[MapFileShare] Command result: " << ret;
 					set_response(_return, true, "Successfully mapped " + mountPoint + " to " + smbShareAddress);
 				}
 				catch (const std::exception& ex) {
@@ -31,25 +33,42 @@ namespace azure {
 				}
 			}
 			void FileShareServiceHandler::UnmapFileShare(LinuxFileResponse& _return, const std::string& mountPoint) {				
-				printf("UnmapFileShare");
+				BOOST_LOG(lg) << "Calling [UnmapFileShare]";
 				try {										
 					bool isUmounted = false;
 					std::string umountCmd = "umount " + mountPoint + " 2>&1";
 					std::string detectCmd = "df -T " + mountPoint + " | tail -n +2";
 					std::string ret;
 					std::chrono::seconds timespan(1);
-					while (!isUmounted) {
+					int maxRetryCount = 5;
+					int attempt = 0;
+					while (attempt < maxRetryCount) {
+						BOOST_LOG(lg) << "[UnmapFileShare] Attempt " << attempt << ":";
+						BOOST_LOG(lg) << "[UnmapFileShare] Excecuting '" << umountCmd << "'";
 						exec(umountCmd.c_str());						
+						BOOST_LOG(lg) << "[UnmapFileShare] Detecting if " << mountPoint << " is unmounted";
 						ret = exec(detectCmd.c_str());
+						BOOST_LOG(lg) << "[UnmapFileShare] Detect result: " << ret;
 						isUmounted = ret.find("cifs") == std::string::npos;
+						BOOST_LOG(lg) << "[UnmapFileShare] " << mountPoint << " state: " << (isUmounted ? "Unmounted" : "Still mounted");
 						if (isUmounted) break;
+						attempt++;
+						BOOST_LOG(lg) << "[UnmapFileShare] Sleeping for " << timespan;
 						std::this_thread::sleep_for(timespan);
 						timespan *= 2;
 					}					
-					std::string removeCmd = "rm -rf " + mountPoint + " 2>&1";										
-					ret = exec(removeCmd.c_str());
-					std::cout << ret << std::endl;
-					set_response(_return, true, "Successfully unmapped " + mountPoint);					
+					if (isUmounted) {
+						BOOST_LOG(lg) << "[UnmapFileShare] Successfully unmounted " << mountPoint << ". Removing it...";						
+						std::string removeCmd = "rm -rf " + mountPoint + " 2>&1";
+						BOOST_LOG(lg) << "[UnmapFileShare] Excecuting '" << removeCmd << "'";
+						ret = exec(removeCmd.c_str());
+						BOOST_LOG(lg) << "[UnmapFileShare] Removal result: " << ret;
+						set_response(_return, true, "Successfully unmapped " + mountPoint);
+					}
+					else {
+						BOOST_LOG(lg) << "[UnmapFileShare] Failed to unmounted " << mountPoint << " after " << maxRetryCount << " attempts";
+						set_response(_return, false, "Failed to unmap " + mountPoint);
+					}
 				}
 				catch (const std::exception& ex) {
 					throw set_exception(ex.what(), OperationType::UnmapFileShare);
--- FileShareServiceHandler.h
+++ FileShareServiceHandler.h
@@ -3,7 +3,42 @@
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/core.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/log/sources/basic_logger.hpp>
+#include <boost/move/utility.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/attributes.hpp>
+#include <boost/log/keywords/format.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/support/date_time.hpp>
+
+using namespace ::apache::thrift;
+using namespace ::apache::thrift::protocol;
+using namespace ::apache::thrift::transport;
+using namespace ::apache::thrift::server;
+
+using boost::shared_ptr;
+namespace expr = boost::log::expressions;
+namespace logging = boost::log;
+namespace keywords = boost::log::keywords;
+using namespace  ::azure::storage::cpp_linux;
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -20,13 +55,28 @@ namespace azure {
 		{
 			class FileShareServiceHandler : virtual public FileShareServiceIf {
 			public:
-				FileShareServiceHandler() {
-
+				FileShareServiceHandler(bool logOn) {
+					if (logOn) {
+						logging::add_file_log(
+							boost::log::keywords::file_name = "map_log/file_%Y-%m-%d_%H-%M-%S.%N.log",
+							boost::log::keywords::rotation_size = 10 * 1024 * 1024,
+							boost::log::keywords::auto_flush = true,
+							boost::log::keywords::format = "[%TimeStamp%]: %Message%"
+							)->locked_backend()->set_file_collector(
+							boost::log::sinks::file::make_collector(
+							boost::log::keywords::target = "/home/xufyan/cpp_linux/map_log",
+							boost::log::keywords::max_size = 0x7fffffff
+							));
+						logging::add_common_attributes();
+					}
 				}
 
 				void MapFileShare(LinuxFileResponse& _return, const std::string& smbShareAddress, const std::string& username, const std::string& password, const std::string& mountPoint);
 
-				void UnmapFileShare(LinuxFileResponse& _return, const std::string& mountPoint);				
+				void UnmapFileShare(LinuxFileResponse& _return, const std::string& mountPoint);			
+
+			private:
+				boost::log::sources::logger lg;
 			};
 		}
 	}
