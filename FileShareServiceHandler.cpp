1395be88c35f662592f943b0952c3442e48d05a1
Xufan Yang <xufyan@microsoft.com>
2016-11-08 16:22:15 +0800

    test

--- FileShareServiceHandler.cpp
+++ FileShareServiceHandler.cpp
@@ -2,6 +2,8 @@
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
 #include <infrastructure.h>
+#include <thread>
+#include <chrono>
 
 using boost::shared_ptr;
 
@@ -11,8 +13,7 @@ namespace azure {
 	namespace storage {
 		namespace cpp_linux
 		{
-			void FileShareServiceHandler::MapFileShare(LinuxFileResponse& _return, const std::string& smbShareAddress, const std::string& username, const std::string& password, const std::string& mountPoint) {
-				// Your implementation goes here
+			void FileShareServiceHandler::MapFileShare(LinuxFileResponse& _return, const std::string& smbShareAddress, const std::string& username, const std::string& password, const std::string& mountPoint) {				
 				printf("MapFileShare");
 				boost::filesystem::path mountPointPath(mountPoint);
 				try {
@@ -30,15 +31,31 @@ namespace azure {
 					throw set_exception(ex.what(), OperationType::MapFileShare);
 				}
 			}
-
-			void FileShareServiceHandler::UnmapFileShare(LinuxFileResponse& _return, const std::string& mountPoint) {
-				// Your implementation goes here
+			void FileShareServiceHandler::UnmapFileShare(LinuxFileResponse& _return, const std::string& mountPoint) {				
 				printf("UnmapFileShare");
 				try {
-					std::string unmountCommand = "umount " + mountPoint;
-					std::string ret = exec(unmountCommand.c_str());
+					std::string cmd = "umount " + mountPoint + " 2>&1";
+					std::string ret = exec(cmd.c_str());
+					std::cout << ret << std::endl;
+					/*
+					bool isUmounted = false;
+					std::string umountCmd = "umount " + mountPoint;
+					std::string detectCmd = "df -T " + mountPoint + " | tail -n +2";
+					std::string ret;
+					std::chrono::seconds timespan(1);
+					while (!isUmounted) {
+						exec(umountCmd.c_str());						
+						ret = exec(detectCmd.c_str());
+						isUmounted = ret.find("cifs") == std::string::npos;
+						if (isUmounted) break;
+						std::this_thread::sleep_for(timespan);
+						timespan *= 2;
+					}					
+					std::string removeCmd = "rm -rf " + mountPoint;										
+					ret = exec(removeCmd.c_str());
 					std::cout << ret << std::endl;
 					set_response(_return, true, "Successfully unmapped " + mountPoint);
+					*/
 				}
 				catch (const std::exception& ex) {
 					throw set_exception(ex.what(), OperationType::UnmapFileShare);
